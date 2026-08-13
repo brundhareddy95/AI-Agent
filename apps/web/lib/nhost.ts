126afb42de03c5afa29c4023e3ad708b985abd70
brundhareddy95 <brundhareddy95@gmail.com>
2026-08-13 20:06:46 +0530

    Add hardcoded fallback for nhost subdomain/region

--- apps/web/lib/nhost.ts
+++ apps/web/lib/nhost.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@nhost/nhost-js";
 
 export const nhost = createClient({
-  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN!,
-  region: process.env.NEXT_PUBLIC_NHOST_REGION!,
+  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || "zvurswochuuxsrfaoebf",
+  region: process.env.NEXT_PUBLIC_NHOST_REGION || "ap-south-1",
 });
